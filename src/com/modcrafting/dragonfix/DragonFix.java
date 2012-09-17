fa54347fd814056d7f26ac45eb716fc5a3dcbacb
V10lator <v10lator@myway.de>
2012-09-17 15:46:14 +0200

    <50 linex... just4fun... ;)

--- src/com/modcrafting/dragonfix/DragonFix.java
+++ src/com/modcrafting/dragonfix/DragonFix.java
@@ -7,56 +7,44 @@ import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class DragonFix extends JavaPlugin implements Listener {
+public class DragonFix extends org.bukkit.plugin.java.JavaPlugin implements org.bukkit.event.Listener {
 	public void onEnable() {
 		for(World w: getServer().getWorlds())
 		   	for(Chunk c: w.getLoadedChunks())
 		   		chunkLoad(new ChunkLoadEvent(c, false));
 		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
 	public void onDisable() {
 		for(World w: getServer().getWorlds())
 	    	for(Chunk c: w.getLoadedChunks())
 	    		chunkUnload(new ChunkUnloadEvent(c));
 	}
-	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void chunkLoad(ChunkLoadEvent event) {
 		for(Entity e: event.getChunk().getEntities())
 			if(e instanceof EnderDragon)
-				replaceEntity((EnderDragon)e, true);
+				((CraftEnderDragon)e).getHandle().X = false;
 	}
-	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void chunkUnload(ChunkUnloadEvent event) {
 		for(Entity e: event.getChunk().getEntities())
 			if(e instanceof EnderDragon)
-				replaceEntity((EnderDragon)e, false);
+				((CraftEnderDragon)e).getHandle().X = true;
 	}
-	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void dragonSpawn(CreatureSpawnEvent event) {
-		Entity e = event.getEntity();
-		if(e instanceof EnderDragon)
-			replaceEntity((EnderDragon)e, true);
+		if(event.getEntity() instanceof EnderDragon)
+			((CraftEnderDragon)event.getEntity()).getHandle().X = false;
 	}
-	
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void dragonExplode(EntityExplodeEvent event)
 	{
 		if(event.getEntity() instanceof EnderDragon)
 			event.blockList().clear();
-	}
-	
-	private void replaceEntity(EnderDragon e, boolean tc) {
-		((CraftEnderDragon)e).getHandle().X = !tc;
 	}
 }
